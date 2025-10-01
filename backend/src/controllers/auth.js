5ffa99c68debd7092985ceaeb6ece15d7c774b2c
Zara khan <zarakhang067@gmail.com>
2025-09-30 19:50:59 -0700

    Initial commit

---
+++ backend/src/controllers/auth.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+  res.send("Hello from signup");
+};
+export const login = (req, res) => {
+  res.send("Hello from login");
+};
+export const logout = (req, res) => {
+  res.send("Hello from logout");
+};
---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.js";
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(" Server is running on port :" + PORT);
+  connectDB();
+});
