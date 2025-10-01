dcabd2a6689bab2048191e9856c4e71e868cd806
Zara khan <zarakhang067@gmail.com>
2025-10-01 10:04:54 -0700

    routers complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,43 @@
+
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.js";
 
-const app = express();
-const PORT = process.env.PORT || 5001;
+import authRoutes from "./routes/auth.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
-app.use("/api/auth", authRoutes);
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.listen(PORT, () => {
-  console.log(" Server is running on port :" + PORT);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
