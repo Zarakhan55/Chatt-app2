8b7ae772d1ce38629d060b8ff59a246501ae297e
Zara khan <zarakhang067@gmail.com>
2025-10-03 10:03:45 -0700

    delpoymnet

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -92,7 +92,7 @@ const ProfilePage = () => {
               </div>
               <div className="flex items-center justify-between py-2">
                 <span>Account Status</span>
-                <span className="text-green-500">Active</span>
+                <span className="text-red-500">Active</span>
               </div>
             </div>
           </div>
